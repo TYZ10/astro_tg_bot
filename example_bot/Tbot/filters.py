6402ed6884026be1c2ebcbddfa69875553916390
TYZ10 <arnautov_0606@mail.ru>
2025-01-27 17:57:52 +0800

    fix

--- example_bot/Tbot/filters.py
+++ example_bot/Tbot/filters.py
@@ -1,14 +1,26 @@
+from datetime import datetime
+
 from aiogram.filters import BaseFilter
 
 from .check_sub import check_sub
-from . import ConfigBot
+from . import ConfigBot, OperationDataBaseBot
 
 
 class CheckSubFilter(BaseFilter):
     """Проверяет на подписку"""
-    def __init__(self, config: ConfigBot):
+    def __init__(self, config: ConfigBot, operation_db: OperationDataBaseBot):
         self.config = config
+        self.operation_db = operation_db
 
     async def __call__(self, message_or_call) -> bool:
         userid = message_or_call.from_user.id
+
+        self.operation_db.update_user_info_db(
+            {
+                self.operation_db.COLUMNS_INFO.last_action:
+                    datetime.now(),
+            },
+            userid
+        )
+
         return not (await check_sub(message_or_call.bot, self.config, userid))
--- example_bot/Tbot/ref_system.py
+++ example_bot/Tbot/ref_system.py
@@ -5,9 +5,22 @@ from . import BasicBotOperation
 
 class MyDataBot(BasicBotOperation):
     async def my_ref_handler(self, message: types.Message):
+
+        col_info = self.operation_db.COLUMNS_INFO
+
+        count_user, count_point = self.operation_db.select_user_info_db(
+            f"{col_info.referral_all_count_user}, "
+            f"{col_info.referral_all_count_points_user}",
+            message.from_user.id
+        )
+
         await message.answer(
             text=f"Ваша реферальная ссылка:\n\n"
-                 f"<code>{message.bot}?start={message.from_user.id}</code>",
+                 f"<code>{message.bot}?start={message.from_user.id}</code>\n\n"
+                 f"Ваша статистика:\n"
+                 f"Количество зарегистрированных рефералов: {count_user}\n"
+                 f"Количество рефералов, которые выполнили генерации. "
+                 f"{count_point}",
             reply_markup=self.keyboard.main_menu_kb,
             parse_mode="HTML"
         )

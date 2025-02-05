4f8cc3c998cfe5e8caeb90f31eeb0b3492d59ea9
TYZ10 <arnautov_0606@mail.ru>
2025-02-05 16:23:59 +0800

    fix

--- example_bot/Tbot/generation/get_info_gpt.py
+++ example_bot/Tbot/generation/get_info_gpt.py
@@ -1,9 +1,11 @@
-from example_bot.misc import generate_gpt_txt, create_aspects
+from datetime import date
+
+from example_bot.misc import generate_gpt_txt
 from example_bot.Tbot import ConfigBot
 
 
 async def main_get_info_gpt(config: ConfigBot, all_info, promt: str):
-    promt = promt + f"\n{all_info}"
+    promt = promt + f"\n{all_info}\n\nСегодняшняя дата: {date.today()}"
 
     text = await generate_gpt_txt(
         config.client_gpt,
